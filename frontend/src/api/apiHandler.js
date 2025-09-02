522ff8d7891688dd19866b48ebf8ab5232a3cf96
Pawan Sonakul <pawansonakul9@gmail.com>
2025-09-02 19:28:59 +0900

    Add files via upload
    new files

---
+++ frontend/src/api/apiHandler.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "https://localhost:8080/api/ratings";
+
+export const addRating = async (ratingData) => {
+  await axios.post("http://localhost:8080/api/ratings", ratingData);
+};
+
+export const fetchAllRatings = async () => {
+  return await axios.get("http://localhost:8080/api/ratings");
+};
+
+export const addContactUs = async (contactData) => {
+  await axios.post("http://localhost:8080/api/admin/contact", contactData);
+};
+
+export const fetchAllContacts = async () => {
+  return await axios.get("http://localhost:8080/api/admin/contact");
+};
