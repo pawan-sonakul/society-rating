94e4b3fda5afb1022d76a91f73f2468b6da89dcf
Pawan Sonakul <pawansonakul9@gmail.com>
2025-09-02 16:14:14 +0530

    Update ContactController.java

--- backend/src/main/java/com/society/rating/controller/ContactController.java
+++ backend/src/main/java/com/society/rating/controller/ContactController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/contact")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ContactController {
     private final ContactServiceImpl contactService;
 
@@ -28,3 +28,4 @@ public class ContactController {
         return ResponseEntity.ok(ratingResponseDTOS);
     }
 }
+
