e00adb5a7b9c91056f42b2be9fd32be316cb4f39
Pawan Sonakul <pawansonakul9@gmail.com>
2025-09-02 21:21:49 +0530

    Delete backend directory

--- backend/src/main/java/com/society/rating/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.society.rating.repository;
-
-import com.society.rating.entity.Contact;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface ContactRepository extends MongoRepository<Contact, UUID> {
-}
