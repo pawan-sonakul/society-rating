d7b0d033b974bceb6143a944d755d58ad5364aa2
Pawan Sonakul <pawansonakul9@gmail.com>
2025-09-02 22:11:19 +0530

    Update SubmitRating.jsx

--- frontend/src/pages/SubmitRating.jsx
+++ frontend/src/pages/SubmitRating.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { addRating } from '../api/apiHandler';
+import { useNavigate } from 'react-router-dom';
 
 const SubmitRating = () => {
   const navigate = useNavigate();
